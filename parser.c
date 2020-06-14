eae2b4e0a6c062986df2f5e97dae6f631f6f512e
Kei Huynh <geegee4iee@gmail.com>
2020-06-14 13:16:51 +0100

    update preprocessor to support Windows

--- parser.c
+++ parser.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mpc.h"
 
-// Global variables
-// Store user's input
+// Windows compiler preprocessor
+#ifdef _WIN32
+#include <string.h>
+
 static char input[2048];
+
+// Fake readline function
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+void add_history(char* unused) {}
+#else
+#include <editline/readline.h>
+#endif
 
 // Declaration
 // Forward Declarations
@@ -732,9 +750,8 @@ int main(int argc, char **argv)
 
     while (1)
     {
-        fputs("keii> ", stdout);
-
-        fgets(input, 2048, stdin);
+        char* input = readline("keii> ");
+        add_history(input);
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, lispy, &r))
@@ -750,8 +767,11 @@ int main(int argc, char **argv)
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
+
+        free(input);
     }
 
+    lenv_del(e);
     mpc_cleanup(6, number, symbol, sexpr, qexpr, expr, lispy);
 
     return 0;
